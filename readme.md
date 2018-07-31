e9aca0f723fc16b57b8048e9452e3de2edcc6fc5
Ziggy <ziggy@ziggyforamerica.com>
2018-07-30 21:26:58 -0400

    Arp arp arp arp arp arp
    (Yes, the puppy is writing docs and stuff.)

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Arp arp arp
+
+This is Ziggy's campaign website, [ZiggyForAmerica.com](https://ziggyforamerica.com).
+
+To fire it up:
+
+```
+npm install
+npm start
+```
+
+PR are not accepted. Belly rubs are.
--- template.js
+++ template.js
@@ -1,19 +1,37 @@
 import nunjucks from 'nunjucks';
 import marked from 'marked';
 
+/**
+ * Mark a string as safe so we can include HTML in the template.
+ * @param  {String} html    You're HTML that should not be escaped
+ * @return {String}
+ */
 function markSafe(html) {
   return new nunjucks.runtime.SafeString(html);
 }
 
 export const manageEnvironment = function(environment) {
-  environment.addFilter('slug', function(str) {
-    return str && str.replace(/\s/g, '-', str).toLowerCase();
-  });
 
+  /**
+   * Markdown filter
+   * @param  {String} str  Markdown string
+   * @return {String}      HTML, marked safe
+   */
   environment.addFilter('markdown', (str) => {
     return markSafe(marked(String(str)));
   });
 
+  /**
+   * Render a template with keyword args passed as context.
+   *
+   * {{ component('my-template.html', title="Hello world", emoji="🐶") }}
+   * 
+   * @param  {String}  template   Path to a Nunjucks template
+   * @param  {Object}  context    Nunjucks will roll all the keyword args
+   *                              into an object automatically. This will
+   *                              be the template's context.
+   * @return {HTML}               Renders included component in the template.
+   */
   environment.addGlobal('component', (template, context) => {
     return markSafe(environment.render(template, context));
   });
