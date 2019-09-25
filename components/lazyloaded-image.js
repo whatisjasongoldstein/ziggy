8ad7e3f3bfcfd1ab0b33a30bb777df078c657319
Jason Goldstein <jason@betheshoe.com>
2019-09-24 21:36:19 -0400

    LazyLoad: Delete unnecessary code, refactor

--- components/lazyloaded-image.js
+++ components/lazyloaded-image.js
@@ -1,64 +1,59 @@
 import { useRef, useEffect, useState } from "react";
 
 /**
- * Found https://billyjacoby.dev/intersection-observer-react-hooks
- * I have no idea why this works...
+ * Inspired by https://billyjacoby.dev/intersection-observer-react-hooks
  */
-export const useIntersectionObserver = (
-  ref,
-  { threshold, root, rootMargin }
-) => {
+export const useIntersectionObserver = ref => {
   // configure the state
   const [state, setState] = useState({
     inView: false,
-    triggered: false,
-    entry: undefined
+    triggered: false
   });
 
-  let observer;
+  // Use this to cache the observer across lifecycles
+  const observerRef = useRef();
 
   // Obviously this only works in the browser
-  if (process.browser) {
-    observer =
-      observer ||
-      new IntersectionObserver(
-        (entries, observerInstance) => {
-          // checks to see if the element is intersecting
-          if (entries[0].intersectionRatio > 0) {
-            // if it is update the state, we set triggered as to not re-observe the element
-            setState({
-              inView: true,
-              triggered: true,
-              entry: observerInstance
-            });
-            // unobserve the element
-            observerInstance.unobserve(ref.current);
-          }
-          return;
-        },
-        {
-          rootMargin: "500px"
+  if (process.browser && !observerRef.current) {
+    console.log("Observer created");
+    observerRef.current = new IntersectionObserver(
+      (entries, observerInstance) => {
+        // checks to see if the element is intersecting
+        if (entries[0].intersectionRatio > 0) {
+          // if it is update the state, we set triggered
+          // as to not re-observe the element
+          setState({
+            inView: true,
+            triggered: true
+          });
+          // unobserve the element
+          observerInstance.unobserve(ref.current);
         }
-      );
+        return;
+      },
+      {
+        rootMargin: "500px"
+      }
+    );
   }
 
   useEffect(() => {
     // check that the element exists, and has not already been triggered
-    if (observer && ref.current && !state.triggered) {
-      observer.observe(ref.current);
+    if (observerRef.current && ref.current && !state.triggered) {
+      observerRef.current.observe(ref.current);
     }
+
+    return function() {
+      observerRef.current.unobserve(ref.current);
+    };
   });
 
-  return [state.inView, state.entry];
+  return [state.inView];
 };
 
 export default function LazyLoadedImage({ src, alt }) {
   const elementRef = useRef(null);
-  const [inView, entry] = useIntersectionObserver(elementRef, {
-    threshold: 0
-  });
-
-  useEffect(() => {}, [inView]);
+  const [inView] = useIntersectionObserver(elementRef);
 
   return (
     <img
