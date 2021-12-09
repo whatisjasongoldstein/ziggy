82440f81035722f768eb6c804887d582217e8fa7
Jason Goldstein <jason@betheshoe.com>
2021-12-09 09:04:33 -0500

    Build

--- docs/_next/static/_5P1z08n5shh8KpU1iTow/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/335-2bffcdf33a0fc429.js","static/chunks/pages/index-7ca92af272d49da2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/cJ88wKH1CrKYAYxexsFZc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/335-2bffcdf33a0fc429.js","static/chunks/pages/index-8ab86033b5574490.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
