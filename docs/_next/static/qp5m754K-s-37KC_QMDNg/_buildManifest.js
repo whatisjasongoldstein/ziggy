c798f04892862d9dfd97e12fa5db93244cce0bcd
Jason Goldstein <jason@betheshoe.com>
2021-12-25 23:06:04 -0500

    Push

---
+++ docs/_next/static/qp5m754K-s-37KC_QMDNg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/151-38c9d1fc7d5c2c22.js","static/css/7cf49ab54b4cf928.css","static/chunks/pages/index-d7096bb9fc7a15bc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
